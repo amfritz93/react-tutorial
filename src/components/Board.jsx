313a90bac7d7cbcac1f0e7f8a012fd2bedca0be2
Alex Fritz <alexfritz@Alexs-MacBook-Air.local>
2025-11-22 12:42:07 -0800

    Add conditional to prevent adding something to filled square

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,12 +6,18 @@ export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     const handleClick = (i) => {
+        if (squares[i]) {
+            return;
+        }
+
         const nextSquares = squares.slice();
+
         if (xIsNext) {
             nextSquares[i] = "X";
         } else {
             nextSquares[i] = "O";
         }
+        
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
     } 
