1f60d9f26ce2350022631a2efde5eacb68292b17
Alex Fritz <alexfritz@Alexs-MacBook-Air.local>
2025-11-22 12:13:01 -0800

    Remove value props from Square component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Square({value}) {
+export default function Square() {
     const [value, setValue] = useState(null);
 
     const handleClick = () => {
